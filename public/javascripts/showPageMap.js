512e0c61b7fdc3cb8c0217a96dfd99c578cb4e52
Rick <rickoscar88@gmail.com>
2023-07-05 17:34:05 +0300

    Fifth commit

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,5 +1,5 @@
 mapboxgl.accessToken = mapToken;
-const map = new mapboxgl.Map({
+let map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/outdoors-v12", // style URL
   center: campground.geometry.coordinates, // starting position [lng, lat]
@@ -8,7 +8,7 @@ const map = new mapboxgl.Map({
 
 map.addControl(new mapboxgl.NavigationControl());
 
-const marker = new mapboxgl.Marker({ color: "red" })
+let marker = new mapboxgl.Marker({ color: "red" })
   .setLngLat(campground.geometry.coordinates) // Update the marker's coordinates
   .setPopup(
     new mapboxgl.Popup({ offset: 25 }).setHTML(
@@ -16,3 +16,11 @@ const marker = new mapboxgl.Marker({ color: "red" })
     )
   )
   .addTo(map);
+
+function updateMarkerPosition(newCoordinates) {
+  marker.setLngLat(newCoordinates); // Update the marker's position
+  map.flyTo({ center: newCoordinates }); // Fly to the new location on the map
+}
+
+const newCoordinates = [lng, lat]; // Update the coordinates with the new values
+updateMarkerPosition(newCoordinates);
--- schemas.js
+++ schemas.js
@@ -22,20 +22,22 @@ const extension = (joi) => ({
   },
 });
 
+const ExtendedJoi = Joi.extend(extension);
+
 module.exports.campgroundSchema = Joi.object({
-  campground: Joi.object({
-    title: Joi.string().required(),
-    price: Joi.number().required().min(0),
-    // image: Joi.string().required(),
-    location: Joi.string().required(),
-    description: Joi.string().required(),
+  campground: ExtendedJoi.object({
+    title: ExtendedJoi.string().required(),
+    price: ExtendedJoi.number().required().min(0),
+    // image: ExtendJoi.string().required(),
+    location: ExtendedJoi.string().required(),
+    description: ExtendedJoi.string().required(),
   }).required(),
-  deleteImages: Joi.array(),
+  deleteImages: ExtendedJoi.array(),
 });
 
 module.exports.reviewSchema = Joi.object({
-  review: Joi.object({
-    rating: Joi.number().required().min(1).max(5),
-    body: Joi.string().required(),
+  review: ExtendedJoi.object({
+    rating: ExtendedJoi.number().required().min(1).max(5),
+    body: ExtendedJoi.string().required(),
   }).required(),
 });
